e3f2c78418de58c30cd0f96fe280990d77bd9cfa
PickledChair <ubatamamoon@gmail.com>
2020-12-09 10:58:54 +0900

    章末問題1.4の解答を、自分なりに深さ優先探索を実装して高速化

--- chapter1/practice1_4/practice1_4.cpp
+++ chapter1/practice1_4/practice1_4.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+#include <array>
+
+int get_row1(std::array<int, 4> arr) {
+    int mult = 1;
+    int result = 0;
+    for (auto&& i: arr) {
+        result = result + i * mult;
+        mult = mult * 10;
+    }
+    return result;
+}
 
 int get_row2(int row0, int row1) {
     return row0 * (row1 % 10);
@@ -43,33 +54,61 @@ bool validate_row6(int row0, int row1) {
 
 int main() {
     int row0 = 100000;
-    int row1 = 999;
+    std::array<int, 4> row1_arr = {{}};   // 左から1の位、10の位、100の位...
+    int idx = 0;                          // 今指し示している位
+    bool finished = false;
 
     for (;;) {
-        if (row1 == 9999) {
-            row0++;
-            row1 = 1000;
+        if (row1_arr[idx] == 9) {
+            if (idx == 0) {
+                row1_arr[idx] = 1;
+                row0++;
+            } else {
+                row1_arr[idx] = 0;
+                idx--;
+                continue;
+            }
         } else {
-            row1++;
+            row1_arr[idx]++;
         }
 
-        if (!validate_row2(row0, row1)) continue;
-        if (!validate_row3(row0, row1)) continue;
-        if (!validate_row4(row0, row1)) continue;
-        if (!validate_row5(row0, row1)) continue;
-        if (validate_row6(row0, row1)) {
-            std::cout << "    " << row0 << std::endl;
-            std::cout << "   *  " << row1 << std::endl;
-            std::cout << "   -------" << std::endl;
-            std::cout << "    " << get_row2(row0, row1) << std::endl;
-            std::cout << "   " << get_row3(row0, row1) << std::endl;
-            std::cout << " " << get_row4(row0, row1) << std::endl;
-            std::cout << " " << get_row5(row0, row1) << std::endl;
-            std::cout << "----------" << std::endl;
-            std::cout << row0 * row1 << std::endl;
+        int row1 = get_row1(row1_arr);
+
+        switch (idx) {
+            case 0:
+                if (validate_row2(row0, row1)) idx++;
+                continue;
+            case 1:
+                if (validate_row3(row0, row1)) idx++;
+                continue;
+            case 2:
+                if (validate_row4(row0, row1)) idx++;
+                continue;
+            case 3:
+                if (validate_row5(row0, row1)) {
+                    if (validate_row6(row0, row1)) {
+                        std::cout << "    " << row0 << std::endl;
+                        std::cout << "   *  " << row1 << std::endl;
+                        std::cout << "   -------" << std::endl;
+                        std::cout << "    " << get_row2(row0, row1) << std::endl;
+                        std::cout << "   " << get_row3(row0, row1) << std::endl;
+                        std::cout << " " << get_row4(row0, row1) << std::endl;
+                        std::cout << " " << get_row5(row0, row1) << std::endl;
+                        std::cout << "----------" << std::endl;
+                        std::cout << row0 * row1 << std::endl;
+                        finished = true;
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+                continue;
+            default:
+                std::cout << "桁が大きすぎます" << std::endl;
+                exit(1);
+        }
+        if (finished) {
             break;
-        } else {
-            continue;
         }
     }
 
