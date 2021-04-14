42ece078448773acb917fb1676875b8c536e89c8
PickledChair <ubatamamoon@gmail.com>
2021-04-14 12:37:57 +0900

    章末問題5.9の解答を追加

---
+++ chapter5/practice5_9/practice5_9.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template<typename T> void chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+    }
+}
+
+const long long INF = 1LL << 60;
+
+int main() {
+    // 入力
+    int N;
+    cin >> N;
+    vector<long long> a(N);
+    for (int i = 0; i < N; ++i) cin >> a[i];
+
+    // 累積和をとる
+    vector<long long> S(N+1, 0);
+    for (int i = 0; i < N; ++i) S[i+1] = S[i] + a[i];
+
+    // DP テーブル
+    vector<vector<long long>> dp(N+1, vector<long long>(N+1, INF));
+
+    // 初期条件（要素１個の部分配列はすでに一つのスライムになっているので、コスト０）
+    for (int i = 0; i < N; ++i) dp[i][i+1] = 0;
+
+    // DP ループ
+    for (int bet = 2; bet <= N; ++bet) {
+        for (int i = 0; i + bet <= N; ++i) {
+            int j = i + bet;
+
+            // dp[i][j] を更新する
+            for (int k = i+1; k < j; ++k) {
+                chmin(dp[i][j], dp[i][k] + dp[k][j] + S[j]-S[i]);
+            }
+        }
+    }
+
+    // 結果
+    cout << dp[0][N] << endl;
+}
