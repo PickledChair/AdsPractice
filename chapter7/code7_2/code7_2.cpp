842cf74e70a9ed6e353a8b604bbba470753df9c4
PickledChair <ubatamamoon@gmail.com>
2021-04-17 22:02:35 +0900

    code7.2 区間スケジューリング問題に対する貪欲法のコード例を追加

---
+++ chapter7/code7_2/code7_2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// 区間を pair<int,int> で表す
+using Interval = pair<int,int>;
+
+// 区間を終端時刻で大小比較する関数
+bool cmp(const Interval &a, const Interval &b) {
+    return a.second < b.second;
+}
+
+int main() {
+    // 入力
+    int N;
+    cin >> N;
+    vector<Interval> inter(N);
+    for (int i = 0; i < N; ++i)
+        cin >> inter[i].first >> inter[i].second;
+
+    // 終端時刻が早い順にソートする
+    sort(inter.begin(), inter.end(), cmp);
+
+    // 貪欲に選ぶ
+    int result = 0;
+    int current_end_time = 0;
+    for (int i = 0; i < N; ++i) {
+        // 最後に選んだ区間と被るものは除く
+        if (inter[i].first < current_end_time) continue;
+
+        ++result;
+        current_end_time = inter[i].second;
+    }
+    cout << result << endl;
+}
